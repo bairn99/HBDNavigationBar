91487b43b5d66b0e212492c5b1743b8c2298c517
lbs <627720876@qq.com>
2024-09-24 22:02:39 +0800

    fix: ios18

--- HBDNavigationBar/Classes/HBDNavigationController.m
+++ HBDNavigationBar/Classes/HBDNavigationController.m
@@ -564,6 +564,7 @@ void printViewHierarchy(UIView *view, NSString *prefix) {
     self.navigationBar.shadowImageView.alpha = 0;
     self.navigationBar.backgroundImageView.alpha = 0;
     // WGMARK: - 解决iOS18透明底问题
+    [self.navigationBar setNeedsLayout];
     [self.navigationBar layoutIfNeeded];
     [self showFakeBarFrom:from];
     [self showFakeBarTo:to];
